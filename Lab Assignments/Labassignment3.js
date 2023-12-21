6d4419ee262dc379d2e52a2d6d78ab7fd5b29437
Ayesha Javeed <114872835+AyeshaJaved006@users.noreply.github.com>
2023-12-21 09:15:44 +0500

    Add files via upload

---
+++ Lab Assignments/Labassignment3.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [comsatsLocation, setComsatsLocation] = useState({
+    latitude: 33.7830, 
+    longitude: 72.3529,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {location && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+            title="Your Location"
+          />
+          <Marker
+            coordinate={comsatsLocation}
+            title="COMSATS Attock"
+          />
+        </MapView>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+  },
+});
